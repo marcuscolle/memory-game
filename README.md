62e4e677a6bf1124190d8ba58c3fb6548bc4a5c4
Ubuntu <ubuntu@ip-172-31-20-194.ec2.internal>
2019-10-02 20:40:33 +0000

    final changes

--- README.md
+++ README.md
@@ -1,15 +1,89 @@
-         ___        ______     ____ _                 _  ___  
-        / \ \      / / ___|   / ___| | ___  _   _  __| |/ _ \ 
-       / _ \ \ /\ / /\___ \  | |   | |/ _ \| | | |/ _` | (_) |
-      / ___ \ V  V /  ___) | | |___| | (_) | |_| | (_| |\__, |
-     /_/   \_\_/\_/  |____/   \____|_|\___/ \__,_|\__,_|  /_/ 
- ----------------------------------------------------------------- 
+# Memory Game - Mile Stone Project II
+
+This game was made to give me a better undertanding of javaScript, it's a 
+normal memory game with card  that has two levels of dificulty. The idea 
+is to match two cards with the same picture. I choose programming language
+to be tha card face and to the back face i've choose code institute logo.
+
+### Ux
+
+The memory game it's a front-end project where you can find a normal 
+card game that match two cards from the same type, its has two levels 
+of dificilty. 
+
+As a gamer i want to finish all levels so that i can complete the first level,
+and click on the next level picture to go to the nextl level. Same thing will happen
+when you finish the other level it appears a Congrats! message and when you click on the picture 
+insted to the next level you have a choice to play again from the begining.
+
+ 
+### Technologies Used
+
+To build this project i have used 
+
+-HTML
+-CSS
+-JAVASCRIPT
+-BOOTSTRAP
+
+### Features
+
+This site is feature with bootstrap4 to give responsive design to different screens sizes.
 
 
-Hi there! Welcome to AWS Cloud9!
+### Features left to implement
 
-To get started, create some files, play with the terminal,
-or visit https://docs.aws.amazon.com/console/cloud9/ for our documentation.
+In the future this game will have one more level of dificulty with 64 cards and 32 matches, also it will
+be implemented a cronometer and a click counter so when you finish the whole game it will appear to the
+user how many clicks they made to complete the game and in how long time.
 
-Happy coding!
-# memory-game
+
+### Testing
+
+The memory game was created to be fun easy and nice to play. When you load the first page
+it will appear 4 x 4 line with 16 cards to flip, so whem you click n the card the card will 
+turn and will show one programming language, the idea is to match the same programming language cards,
+this first page will have 8 matches and on every card match a little data sign will appear very fast to 
+interaction with the user and show them they got one match. Notice that the user can give one click per card
+that will flip that card. When the user finish to flip all the cards and match them a modal screen
+will appear with a picture to go to the next level.
+
+On the next level it will appear 32 cards with a total of 16 matches. In this level the user
+will have the same experience as the first level, but, harder and longer to finish. When all
+matches is obtained another modal screen will appear giving user congrats! and a restat sign
+to start the game from the first level in case of the user wants to play again.
+
+This site has been tested as responvise design in multiples devices as Desktop, Tablets and Phones.
+
+
+### Deployment
+
+In order to view this project you can get and the codes in my git-hub space and run in a terminal:
+
+https://github.com/marcuscolle/memory-game
+
+to make it run property the landing page is named index.html
+
+
+### Credits
+
+I would like to give some credits to the youtube video that have made me understand how
+this memory game works specially with javascript. From that video i have picked up the 
+memory game idea and from that i have made some adapitations specially to the javascript 
+code where i have implemented another level of dificulty and diferent sizes of boards and card,
+as well diferent front and back pictures.
+
+I would like to give some credits to my Mentor Aaron Sinnott that helped me during the whole project.
+
+#### Content
+
+My memory game was based in the youtube on: https://www.youtube.com/watch?v=U0I6Z06N0kM&list=PLclUTiUoLCbC7ynbckOgMO0sXHh5uXVv8&index=1
+
+
+#### Media 
+
+All the pictures where picked up from google images searching programming languages logo and codo institute logo.
+
+ 
+
+
--- js/script.js
+++ js/script.js
@@ -1,242 +1,146 @@
+let matches = 0; // count the hits
+let images = []; // created to add images to the cards
+let images1 = []; // created to add img to the medium level board
+let flippedCards = [];
+const modalGameOver = document.querySelector('#modalGameOver');
+const imgMatchSign = document.querySelector('#imgMatchSign'); // var created to reference the element.
+const easy = document.getElementById('board');
+const medium = document.getElementById('board1');
 
-    var matches = 0; // count the hits
-    
-    var matches1 = 0;
-
-    var images = []; //created to add images to the cards
-    
-    var images1 = [];
-
-    var flippedCards = [];
-
-    var modalGameOver = document.querySelector("#modalGameOver");
-    
-    var imgMatchSign = document.querySelector("#imgMatchSign"); // var created to reference the element.
-    
-    
-     /*-----------------------*/
-            var easy = document.getElementById("board");
-         /*------------------------*/  
-            var medium = document.getElementById("board1");
-    
-    
-    
- 
-    
-    for (var i = 0; i < 16; i++) {
-       
-       if(images){
-         var img = {
-                src: "img/" + i + ".png",
-                id: i % 8
+for (var i = 0; i < 16; i++) {
+    if (images) {
+        var img = {
+            src: `img/${i}.png`,
+            id: i % 8,
         };
-       }
-       images.push(img);
-    }    
-        
-        for (var i = 0; i < 32; i++){
-         if(images1){
-            var img = {
-                src: "medium/" + i + ".jpg",
-                id: i % 16
-            };
-        }
-        
-        images1.push(img);
-    } 
-   
-
-    startGame(); // calling the funciton to start the game
-
-    //function responsible to start the game
-    function startGame() {
-        
-        matches = 0; //reset our hit counter so the game can restart again when click on gameOver
-        matches1 =0;
-        
-        flippedCards = []; // makes the game start with empty array
-        
-        
-
-        images = randomSort(images); //random function to ramdom the cards inside the board
-        images1 = randomSort(images1);
-
-        var frontFaces = document.getElementsByClassName("front");
-        var backFaces = document.getElementsByClassName("back");
-
-        
-       
-        for (var i = 0; i < 32; i++) { //for looping to distribute the cards inside the container ---  while is less than 16 will increment 1 by 1 
-           
-
-            frontFaces[i].classList.remove("flipped", "match"); //removing the classes to original configuration to restart the match without any problem.
-            backFaces[i].classList.remove("flipped", "match");
-
-            var card = document.getElementById("card" + i); // #card + i = idcard plus his number on html
-            
-            card.addEventListener("click", flipCard, false);
-             
-       //     frontFaces[i].style.background = "url('" + images[i].src + "')"; // creates all cards images to mach the pictures
-
-           
-            
-            if(easy){
-            card.style.left = (i === 0 || i=== 4) ?/*if yes =*/ 5 + "px" : i % 4 * 105 + 5 + "px";
-            card.style.top = i < 4 ? 5 + "px" : i < 8 ? 130 + "px" : i < 12 ? 255 + "px" : 380 + "px";
-            
-             frontFaces[i].style.background = "url('" + images[i].src + "')"; // creates all cards images to mach the pictures
-             frontFaces[i].setAttribute("id", images[i].id); // this will match the cards using the id - id will have values from 0-7 or 0-15;
-            }
-            
-            if (medium){
-            
-            card.style.left = i % 8 === 0 ? 5 + "px" : i % 8 * 105 + 5 + "px";
-            card.style.top = i < 8 ? 5 + "px" : i < 16 ? 130 + "px" : i < 24 ? 255 + "px" : 380 + "px"; //positionig the cards
-            
-              frontFaces[i].style.background = "url('" + images1[i].src + "')"; // creates all cards images to mach the pictures
-              frontFaces[i].setAttribute("id", images1[i].id); // this will match the cards using the id - id will have values from 0-7 or 0-15;
-            }
-           
-             // frontFaces[i].setAttribute("id", images[i].id); // this will match the cards using the id - id will have values from 0-7 or 0-15;
-           
-        }
-        
-        
-   /*     for (var i = 0; i < 32; i++) { //for looping to distribute the cards inside the container ---  while is less than 16 will increment 1 by 1 
-            // by every indice we will have a var card that reffer a #card (i could use document.getElementById()).
-
-            frontFaces[i].classList.remove("flipped", "match"); //removing the classes to original configuration to restart the match without ant problem.
-            backFaces[i].classList.remove("flipped", "match");
-
-            card.style.left = i % 8 === 0 ? 5 + "px" : i % 8 * 165 + 5 + "px";
-            card.style.top = i < 8 ? 5 + "px" : i < 16 ? 180 + "px" : i < 24 ? 355 + "px" : 530 + "px"; //positionig the cards
-
-            card.addEventListener("click", flipCard, false);
-
-            frontFaces[i].style.background = "url('" + images[i].src + "')"; // creates all cards images to mach the pictures
-
-            frontFaces[i].setAttribute("id", images[i].id); // this will match the cards using the id - id will have values from 0-7. 
-        }
-
-      */  
-
-        modalGameOver.style.zIndex = -2;
-        modalGameOver.removeEventListener("click", startGame, false);
-
     }
-    
-    
+    images.push(img);
+}
 
-    function randomSort(oldArray) {
-        // 1 -Math.floor(Math.random()*11);  // Math.floor = round down number - Math.random = random number - *11 will give a integer numbers between 0 and 10.999 + Math.floor = 0 to 10 integer numbers.
-        //2 - var arrTest = ["banana", "morango", "maca"]
-        //console.log(arrTest.indexOf("banana"));
-        //3- console.log(arrTest.lenght()) -- return the number of elements of the array
+for (var i = 0; i < 32; i++) {
+    if (images1) {
+        var img = {
+            src: `medium/${i}.jpg`,
+            id: i % 16,
+        };
+    }
+    images1.push(img);
+}
 
-        var newArray = [];
+startGame(); // calling the funciton to start the game
 
-        while (newArray.length !== oldArray.length) {
-            var i = Math.floor(Math.random() * oldArray.length); // this will create an radom numbers between 0 - 15 because we have 16 cards in the game
+// function responsible to start the game
+function startGame() {
+    matches = 0; // reset our hit counter
+    flippedCards = []; // makes the game start with empty array
+    images = randomSort(images); // random function to random the cards inside the board
+    images1 = randomSort(images1);
 
-            if (newArray.indexOf(oldArray[i]) < 0) {
-                newArray.push(oldArray[i]);
-            }
+    const frontFaces = document.getElementsByClassName('front');
+    const backFaces = document.getElementsByClassName('back');
+
+    for (let i = 0; i < 32; i++) { // for looping to distribute the cards inside the container 
+        frontFaces[i].classList.remove('flipped', 'match'); // removing the classes to original configuration to restart the match without any problem.
+        backFaces[i].classList.remove('flipped', 'match');
+
+        const card = document.getElementById(`card${i}`); // #card + i = idcard plus his number on html
+        card.addEventListener('click', flipCard, false);
+      
+        if (easy) {
+            card.style.left = (i === 0 || i === 4) ? /* if yes = */ `${5}px` : `${i % 4 * 105 + 5}px`;
+            card.style.top = i < 4 ? `${5}px` : i < 8 ? `${130}px` : i < 12 ? `${255}px` : `${380}px`;
+
+            frontFaces[i].style.background = `url('${images[i].src}')`; // make all cards images to mach the pictures
+            frontFaces[i].setAttribute('id', images[i].id); // this will match the cards using the id - id will have values from 0-7 or 0-15;
         }
 
-        return newArray;
+        if (medium) {
+            card.style.left = i % 8 === 0 ? `${5}px` : `${i % 8 * 105 + 5}px`;
+            card.style.top = i < 8 ? `${5}px` : i < 16 ? `${130}px` : i < 24 ? `${255}px` : `${380}px`; // positionig the cards
+
+            frontFaces[i].style.background = `url('${images1[i].src}')`; // make all cards images to mach the pictures
+            frontFaces[i].setAttribute('id', images1[i].id); // this will match the cards using the id - id will have values from 0-7 or 0-15;
+        }
+        
     }
+    modalGameOver.style.zIndex = -2;
+}
 
-    //to flipp cards and turn only 2 cards.
-    function flipCard() {
-        
-        
-        //turning only 2 cards
-        if (flippedCards.length < 2) {
-            var faces = this.getElementsByClassName("face");
+function randomSort(oldArray) {
+    const newArray = [];
 
-            if (faces[0].classList.length > 2) { //bug fix only one click by card
+    while (newArray.length !== oldArray.length) {
+        const i = Math.floor(Math.random() * oldArray.length); // this will create an random numbers between 0 - 15 
 
-                return; //dont let the card turn.
-            }
+        if (newArray.indexOf(oldArray[i]) < 0) {
+            newArray.push(oldArray[i]);
+        }
+    }
+    return newArray;
+}
 
-            faces[0].classList.toggle("flipped"); //[0] means back face to flipp the card
-            faces[1].classList.toggle("flipped"); // [1] front face 
+// to flipp cards and turn only 2 cards.
+function flipCard() {
+    // turning only 2 cards
+    if (flippedCards.length < 2) {
+        const faces = this.getElementsByClassName('face');
 
-            flippedCards.push(this);
+        if (faces[0].classList.length > 2) { // bug fix only one click by card
+            return; // dont let the same card turn and turn back again. 
+        }
 
-            if (flippedCards.length === 2) { //check if my array already has 2 cards and see if they combine
-                if (flippedCards[0].childNodes[3].id === flippedCards[1].childNodes[3].id) { // index [1] and [3] took it from childNode 
-                        flippedCards[0].childNodes[1].classList.toggle("match"); 
-                        flippedCards[0].childNodes[3].classList.toggle("match");
-                        flippedCards[1].childNodes[1].classList.toggle("match");//all cards that combine will receive a class match with box shadow
-                        flippedCards[1].childNodes[3].classList.toggle("match");
-                        
-           
-                        matchCardSign(); // when match appear a little data on screen
-                        
-                        
-                        matches++; //counter incremented by 1
-                        matches1++;
-                     
-                        flippedCards = []; //keep array zero
-                        
-                       
-                     /*-----------------------------------------------------------*/  
-                       
-                       
-                         if(matches === 8){ // if all cards have been turned or 8 matches 
-                            nextLevel ();
-                        }
-                        
-                         if(matches === 16){
-                             gameOver();//if yes calling gameover function.
-                        }
-                        
-                      
-                    /*-----------------------------------------------------------*/    
-                       
-                        
-                        
+        faces[0].classList.toggle('flipped'); // [0] back face with flipped class added to it
+        faces[1].classList.toggle('flipped'); // [1] front face with flipped class added to it
+
+        flippedCards.push(this);
+
+        if (flippedCards.length === 2) { // check if my array already has 2 cards and see if they combine
+            if (flippedCards[0].childNodes[3].id === flippedCards[1].childNodes[3].id) { // index [1] and [3] took it from childNode
+                flippedCards[0].childNodes[1].classList.toggle('match');
+                flippedCards[0].childNodes[3].classList.toggle('match');
+                flippedCards[1].childNodes[1].classList.toggle('match'); // all cards that combine will receive a class match with box shadow
+                flippedCards[1].childNodes[3].classList.toggle('match');
+
+                matchCardSign(); // when match appears a little data on screen
+                matches++; // counter incremented by 1
+                flippedCards = []; // keep array zero
+
+            //all cards match "function" to finish or next level game 
+                if (medium && matches === 16) {
+                    gameOver(); // if yes calling gameover function.
                 }
 
+                if (easy && matches === 8) { // if all cards have been turned or 8 matches modal screen appear.
+                    nextLevel(); 
+                }
             }
-
-        }else { //arrays [1][3] - console.log(flippedCards) = childNode
-            flippedCards[0].childNodes[1].classList.toggle("flipped");
-            flippedCards[0].childNodes[3].classList.toggle("flipped");
-            flippedCards[1].childNodes[1].classList.toggle("flipped");
-            flippedCards[1].childNodes[3].classList.toggle("flipped");
-
-            flippedCards = [];
         }
-
-
-
     }
+    else { // arrays [1][3] - console.log(flippedCards) = childNode
+        flippedCards[0].childNodes[1].classList.toggle('flipped');
+        flippedCards[0].childNodes[3].classList.toggle('flipped');
+        flippedCards[1].childNodes[1].classList.toggle('flipped');
+        flippedCards[1].childNodes[3].classList.toggle('flipped');
+        flippedCards = [];
+    }
+}
+// modal function
+function gameOver() {
+    modalGameOver.style.zIndex = 10; // to come the modal screen to the front. z-index
+}
+// match sign function - to appear the data icon when match cards
+function matchCardSign() {
+    imgMatchSign.style.zIndex = 1;
+    imgMatchSign.style.top = `${150}px`; // styling img match to come and go from our screen
+    imgMatchSign.style.opacity = 0;
 
+    setTimeout(() => { // restore the initial settings of the hit so when hit appears again
+        imgMatchSign.style.zIndex = -1;
+        imgMatchSign.style.top = `${250}px`;
+        imgMatchSign.style.opacity = 1;
+    }, 100); // 1500 miliseconds trasition time
+}
 
-    //modal function 
-    function gameOver() {
-        modalGameOver.style.zIndex = 10; //to come the modal screen to the front. z-index
-        modalGameOver.addEventListener("click", startGame, false);
-    }
-    
-    
-       
-    //match sign function - to appear the data icon when cards match
-    function matchCardSign(){
-        imgMatchSign.style.zIndex = 1;
-        imgMatchSign.style.top = 150 + "px";  //styling img match to come and go from our screen
-        imgMatchSign.style.opacity = 0;
-        
-        setTimeout(function(){ // restore the initial settings of the hit so when hit appears again
-            imgMatchSign.style.zIndex = -1;
-            imgMatchSign.style.top = 250 + "px";
-            imgMatchSign.style.opacity = 1;
-        },100);//1500 miliseconds trasition time
-    }
-    
-    function nextLevel (){
-        modalGameOver.style.zIndex = 10;
-        
-    }
+function nextLevel() {
+    modalGameOver.style.zIndex = 10;
+}
